d1c5a0c900f9b0f7af163ea4a6919c62eb55c4d3
Anastasiya96 <razumovskaja-nastja@rambler.ru>
2017-03-16 22:18:03 +0300

    Add cinemas and sessions

--- create_documents.js
+++ create_documents.js
@@ -16,80 +16,84 @@ db.createCollection('persons', {
 db.persons.createIndex({name: 1});
 
 db.createCollection('films', {
-    validator: {
-        $and: [
-            { name: {$type: 'string'} },
-            {
-                producers: [
-                    { person_id: {$type: 'string'} }
-                ]
-            },
-            { year: {$type: 'int'} },
-            { duration: {$type: 'int'} },
-            {
-                countries: [
-                    { name: {$type: 'string'} }
-                ]
-            },
-            { rating: {$type: 'double'} },
-            {
-                genres: [
-                    { name: {$type: 'string'} }
-                ]
-            },
-            { description: {$type: 'string'} },
-            {
-                actors: [
-                    { person_id: {$type: 'string'} }
-                ]
-            },
-            {
-                reviews: [
-                    { review_id: {$type: 'string'} }
-                ]
-            }
-        ]
+        validator: {
+            $and: [
+                { name: {$type: 'string'} },
+                {
+                    producers: [
+                        { person_id: {$type: 'string'} }
+                    ]
+                },
+                { year: {$type: 'int'} },
+                { duration: {$type: 'int'} },
+                {
+                    countries: [
+                        { name: {$type: 'string'} }
+                    ]
+                },
+                { rating: {$type: 'double'} },
+                {
+                    genres: [
+                        { name: {$type: 'string'} }
+                    ]
+                },
+                { description: {$type: 'string'} },
+                {
+                    actors: [
+                        { person_id: {$type: 'string'} }
+                    ]
+                },
+                {
+                    reviews: [
+                        { review_id: {$type: 'string'} }
+                    ]
+                }
+            ]
+        }
     }
-}
+);
 db.films.createIndex({name: 1});
 db.films.createIndex({rating: 1});
 
 db.createCollection('users', {
-    validator: {
-        $and: [
-            { nickname: {$type: 'string'} },
-            { email: {$type: 'string'} },
-            { password: {$type: 'string'} },  //hash?! auth
-            { birthday: {$type: 'date'} },
-            { registration_date: {$type: 'date'} },
-            { country: {$type: 'string'} },
-            { city: {$type: 'string'} },
-            {
-                reviews: [
-                    { review_id: {$type: 'string'} }
-                ]
-            },
-            {
-                friends: [
-                    { user_id: {$type: 'string'} }
-                ]
-            }
-        ]
+        validator: {
+            $and: [
+                { nickname: {$type: 'string'} },
+                { email: {$type: 'string'} },
+                { password: {$type: 'string'} },  //hash?! auth
+                { birthday: {$type: 'date'} },
+                { registration_date: {$type: 'date'} },
+                { country: {$type: 'string'} },
+                { city: {$type: 'string'} },
+                {
+                    reviews: [
+                        { review_id: {$type: 'string'} }
+                    ]
+                },
+                {
+                    friends: [
+                        { user_id: {$type: 'string'} }
+                    ]
+                }
+            ]
+        }
     }
-}
+);
 db.users.createIndex({nickname: 1});
 
 db.createCollection('reviews', {
-    validator: {
-        $and: [
-            { mark: {$type: 'int'} },
-            { review: {$type: 'string'} },
-            { date: {$type: 'date'} },
-            { author_id: {$type: 'string'} },
-            { film_id: {$type: 'string'} }
-        ]
+        validator: {
+            $and: [
+                { mark: {$type: 'int'} },
+                { review: {$type: 'string'} },
+                { date: {$type: 'date'} },
+                { author_id: {$type: 'string'} },
+                { film_id: {$type: 'string'} }
+            ]
+        }
     }
-}
+);
+db.reviews.createIndex({film_id: 1});
 
 db.createCollection("cinemas", {
         validator: {
