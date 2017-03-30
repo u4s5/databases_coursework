1d7206c8632ad3c4086e2192dbd8f5fa248ebbf4
u4s5 <u4s5@yandex.ru>
2017-03-31 01:20:36 +0300

    Add Mongo generation

--- databasesCoursework/src/main/java/MongoFill.java
+++ databasesCoursework/src/main/java/MongoFill.java
@@ -10,9 +10,9 @@ import java.util.Random;
 
 public class MongoFill {
 
-    private static final int MOVIES_COUNT = 1000;
-    private static final int PEOPLE_COUNT = 2000;
-    private static final int REVIEWS_COUNT = 2000;
+    private static final int MOVIES_COUNT = 1000000;
+    private static final int PEOPLE_COUNT = 2000000;
+    private static final int REVIEWS_COUNT = 2000000;
 
     public static void main(String[] args) throws IOException {
         // connect to database
