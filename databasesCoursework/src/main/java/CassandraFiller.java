af3e8781d942fdc943d38a07c7c66257ca5ef861
u4s5 <u4s5@yandex.ru>
2017-04-23 15:37:50 +0300

    Decrease amount of records

--- databasesCoursework/src/main/java/CassandraFiller.java
+++ databasesCoursework/src/main/java/CassandraFiller.java
@@ -7,9 +7,9 @@ import java.util.Random;
 
 public class CassandraFiller {
 
-    private static final int MOVIES_COUNT = 1000000;
-    private static final int PEOPLE_COUNT = 2000000;
-    private static final int REVIEWS_COUNT = 2000000;
+    private static final int MOVIES_COUNT = 250000;
+    private static final int PEOPLE_COUNT = 500000;
+    private static final int REVIEWS_COUNT = 500000;
 
     public static void fill() {
 
