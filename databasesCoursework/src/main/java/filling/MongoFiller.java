b0977b6a6528cdc3e74c6b136202f4272c1dbd5e
u4s5 <u4s5@yandex.ru>
2017-04-30 14:45:25 +0300

    Rewrite filling of Mongo with multithreading. Edit pom.xml to generate .jar

--- databasesCoursework/src/main/java/filling/MongoFiller.java
+++ databasesCoursework/src/main/java/filling/MongoFiller.java
@@ -29,74 +29,82 @@ public class MongoFiller {
         db.drop();
 
         Random random = new Random();
-        Document document;
 
-        // fill collection of people
-        MongoCollection collection = db.getCollection("people");
-        for (int i = 0; i < PEOPLE_COUNT; i++) {
-            document = new Document("_id", i)
-                    .append("name", RandomGenerator.generateName())
-                    .append("birthday", RandomGenerator.generateBirthday())
-                    .append("country", RandomGenerator.generateCountry());
+        new Thread(() -> {
+            // fill collection of people
+            MongoCollection collection = db.getCollection("people");
+            Document document;
+            for (int i = 0; i < PEOPLE_COUNT; i++) {
+                document = new Document("_id", i)
+                        .append("name", RandomGenerator.generateName())
+                        .append("birthday", RandomGenerator.generateBirthday())
+                        .append("country", RandomGenerator.generateCountry());
 
-            List<String> occupations = new ArrayList<>();
-            if (i < PEOPLE_COUNT / 2)
-                occupations.add("actor");
-            else
-                occupations.add("producer");
+                List<String> occupations = new ArrayList<>();
+                if (i < PEOPLE_COUNT / 2)
+                    occupations.add("actor");
+                else
+                    occupations.add("producer");
 
-            document.append("occupations", occupations);
+                document.append("occupations", occupations);
 
+                collection.insertOne(document);
+            }
+        }).start();
+
+        new Thread(() -> {
+            // fill collection of films
+            MongoCollection collection = db.getCollection("films");
+            Document document;
+            for (int i = 0; i < MOVIES_COUNT; i++) {
+                document = new Document("_id", i)
+                        .append("name", RandomGenerator.generateName())
+                        .append("year", RandomGenerator.generateYear())
+                        .append("duration", RandomGenerator.generateDuration())
+                        .append("countries", new ArrayList<String>() {{
+                            add(RandomGenerator.generateCountry());
+                        }})
+                        .append("rating", RandomGenerator.generateRating())
+                        .append("genres", RandomGenerator.generateGenres())
+                        .append("description", RandomGenerator.generateText(150))
+                        .append("producer_ids", new ArrayList<Integer>() {{
+                            add(random.nextInt(PEOPLE_COUNT / 2) + PEOPLE_COUNT / 2);
+                        }})
+                        .append("actor_ids", new ArrayList<Integer>() {{
+                            for (int i = 0; i < 5; i++)
+                                add(random.nextInt(PEOPLE_COUNT / 2));
+                        }});
+
+                collection.insertOne(document);
+            }
+        }).start();
+
+        new Thread(() -> {
+            // fill collection of reviews
+            MongoCollection collection = db.getCollection("reviews");
+            Document document;
+            for (int i = 0; i < REVIEWS_COUNT; i++) {
+                document = new Document("_id", i)
+                        .append("author", RandomGenerator.generateName())
+                        .append("mark", (int) (Math.round(RandomGenerator.generateRating())))
+                        .append("date", RandomGenerator.generateDate())
+                        .append("text", RandomGenerator.generateText(300))
+                        .append("film_id", random.nextInt(MOVIES_COUNT));
+
+                collection.insertOne(document);
+            }
+        }).start();
+
+        new Thread(() -> {
+            // fill collection of id-counters
+            MongoCollection collection = db.getCollection("counters");
+            Document document = new Document("_id", "personId").append("next", PEOPLE_COUNT);
             collection.insertOne(document);
-        }
-
-        // fill collection of films
-        collection = db.getCollection("films");
-        for (int i = 0; i < MOVIES_COUNT; i++) {
-            document = new Document("_id", i)
-                    .append("name", RandomGenerator.generateName())
-                    .append("year", RandomGenerator.generateYear())
-                    .append("duration", RandomGenerator.generateDuration())
-                    .append("countries", new ArrayList<String>() {{
-                        add(RandomGenerator.generateCountry());
-                    }})
-                    .append("rating", RandomGenerator.generateRating())
-                    .append("genres", RandomGenerator.generateGenres())
-                    .append("description", RandomGenerator.generateText(150))
-                    .append("producer_ids", new ArrayList<Integer>() {{
-                        add(random.nextInt(PEOPLE_COUNT / 2) + PEOPLE_COUNT / 2);
-                    }})
-                    .append("actor_ids", new ArrayList<Integer>() {{
-                        for (int i = 0; i < 5; i++)
-                            add(random.nextInt(PEOPLE_COUNT / 2));
-                    }});
-
+            document = new Document("_id", "filmId").append("next", MOVIES_COUNT);
             collection.insertOne(document);
-        }
-
-        // fill collection of reviews
-        collection = db.getCollection("reviews");
-        for (int i = 0; i < REVIEWS_COUNT; i++) {
-            document = new Document("_id", i)
-                    .append("author", RandomGenerator.generateName())
-                    .append("mark", (int) (Math.round(RandomGenerator.generateRating())))
-                    .append("date", RandomGenerator.generateDate())
-                    .append("text", RandomGenerator.generateText(300))
-                    .append("film_id", random.nextInt(MOVIES_COUNT));
-
+            document = new Document("_id", "reviewId").append("next", REVIEWS_COUNT);
             collection.insertOne(document);
-        }
-
-        // fill collection of id-counters
-        collection = db.getCollection("counters");
-        document = new Document("_id", "personId").append("next", PEOPLE_COUNT);
-        collection.insertOne(document);
-        document = new Document("_id", "filmId").append("next", MOVIES_COUNT);
-        collection.insertOne(document);
-        document = new Document("_id", "reviewId").append("next", REVIEWS_COUNT);
-        collection.insertOne(document);
-
-        mongoClient.close();
+        }).start();
     }
 
 }
