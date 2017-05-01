6b0a7ee77beb249bb1b557c0edd716f95585c99d
u4s5 <u4s5@yandex.ru>
2017-05-01 15:47:25 +0300

    Rewrite filling of PostgreSQL with multithreading

--- databasesCoursework/src/main/java/filling/PostgreSQLFiller.java
+++ databasesCoursework/src/main/java/filling/PostgreSQLFiller.java
@@ -16,15 +16,12 @@ public class PostgreSQLFiller {
 
         Random random = new Random();
 
-        Connection connection;
-        Statement statement;
-
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager
+            Connection connection = DriverManager
                     .getConnection("jdbc:postgresql://aws-us-east-1-portal.5.dblayer.com:18951/compose",
                             "admin", "qwer1234");
-            statement = connection.createStatement();
+            Statement statement = connection.createStatement();
 
             statement.execute("DROP SCHEMA IF EXISTS moviefinder CASCADE");
             statement.execute("CREATE SCHEMA moviefinder");
@@ -38,84 +35,145 @@ public class PostgreSQLFiller {
             statement.execute("CREATE TABLE moviefinder.films_people(id SERIAL, film INT, person INT, kind TEXT)");
             statement.execute("CREATE TABLE moviefinder.films_reviews(id SERIAL, film INT, review INT)");
             statement.execute("CREATE TABLE moviefinder.counters(name TEXT, next_id INT)");
-
-            // filling people
-            for (int i = 0; i < PEOPLE_COUNT; i++) {
-                statement.execute("INSERT INTO moviefinder.people VALUES (" +
-                        i + ",\'" +
-                        RandomGenerator.generateName() + "\',\'" +
-                        RandomGenerator.generateBirthday() + "\',\'" +
-                        RandomGenerator.generateCountry() + "\'," +
-                        (i < PEOPLE_COUNT / 2 ? "\'{\"actor\"}\'" : "\'{\"producer\"}\'") +
-                        "); ");
-            }
-
-            // filling films
-            for (int i = 0; i < MOVIES_COUNT; i++) {
-                List<String> genres = RandomGenerator.generateGenres();
-                statement.execute("INSERT INTO moviefinder.films VALUES (" +
-                        i + ",\'" +
-                        RandomGenerator.generateName() + "\'," +
-                        RandomGenerator.generateYear() + "," +
-                        RandomGenerator.generateDuration() + ",\'" +
-                        RandomGenerator.generateText(150) + "\'," +
-                        RandomGenerator.generateRating().floatValue() + ",\'" +
-                        RandomGenerator.generateCountry() + "\'," +
-                        "\'{\"" + genres.get(0) + "\",\"" + genres.get(1) + "\",\"" + genres.get(2) + "\"}\'" +
-                        "); ");
-            }
-
-            // filling reviews
-            for (int i = 0; i < REVIEWS_COUNT; i++) {
-                statement.execute("INSERT INTO moviefinder.reviews VALUES (" +
-                        i + ",\'" +
-                        RandomGenerator.generateDate() + "\'," +
-                        (random.nextInt(10) + 1) + ",\'" +
-                        RandomGenerator.generateText(300) + "\'" +
-                        "); ");
-            }
-
-            // filling connections
-            for (int i = 0; i < MOVIES_COUNT; i++) {
-                statement.execute("INSERT INTO moviefinder.films_people(film, person, kind) VALUES (" +
-                        i + "," +
-                        (random.nextInt(PEOPLE_COUNT / 2) + PEOPLE_COUNT / 2) +
-                        ",\'producer\'" +
-                        "); ");
-                for (int j = 0; j < 3; j++) {
-                    statement.execute("INSERT INTO moviefinder.films_people(film, person, kind) VALUES (" +
-                            i + "," +
-                            (random.nextInt(PEOPLE_COUNT / 2)) +
-                            ",\'actor\'" +
-                            "); ");
-                }
-            }
-
-            for (int i = 0; i < REVIEWS_COUNT; i++) {
-                statement.execute("INSERT INTO moviefinder.films_reviews(film, review) VALUES (" +
-                        i + "," +
-                        random.nextInt(MOVIES_COUNT) +
-                        "); ");
-            }
-
-            // filling id-counters
-            statement.execute("INSERT INTO moviefinder.counters(name, next_id) VALUES (" +
-                    "\'films\'," +
-                    MOVIES_COUNT +
-                    "); ");
-            statement.execute("INSERT INTO moviefinder.counters(name, next_id) VALUES (" +
-                    "\'people\'," +
-                    PEOPLE_COUNT +
-                    "); ");
-            statement.execute("INSERT INTO moviefinder.counters(name, next_id) VALUES (" +
-                    "\'reviews\'," +
-                    REVIEWS_COUNT +
-                    "); ");
-
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(0);
         }
+
+        new Thread(() -> {
+            // filling people
+            try {
+                Class.forName("org.postgresql.Driver");
+                Connection connection = DriverManager
+                        .getConnection("jdbc:postgresql://aws-us-east-1-portal.5.dblayer.com:18951/compose",
+                                "admin", "qwer1234");
+                Statement statement = connection.createStatement();
+
+                for (int i = 0; i < PEOPLE_COUNT; i++) {
+                    statement.execute("INSERT INTO moviefinder.people VALUES (" +
+                            i + ",\'" +
+                            RandomGenerator.generateName() + "\',\'" +
+                            RandomGenerator.generateBirthday() + "\',\'" +
+                            RandomGenerator.generateCountry() + "\'," +
+                            (i < PEOPLE_COUNT / 2 ? "\'{\"actor\"}\'" : "\'{\"producer\"}\'") +
+                            "); ");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }).start();
+
+        new Thread(() -> {
+            // filling films
+            try {
+                Class.forName("org.postgresql.Driver");
+                Connection connection = DriverManager
+                        .getConnection("jdbc:postgresql://aws-us-east-1-portal.5.dblayer.com:18951/compose",
+                                "admin", "qwer1234");
+                Statement statement = connection.createStatement();
+
+                for (int i = 0; i < MOVIES_COUNT; i++) {
+                    List<String> genres = RandomGenerator.generateGenres();
+                    statement.execute("INSERT INTO moviefinder.films VALUES (" +
+                            i + ",\'" +
+                            RandomGenerator.generateName() + "\'," +
+                            RandomGenerator.generateYear() + "," +
+                            RandomGenerator.generateDuration() + ",\'" +
+                            RandomGenerator.generateText(150) + "\'," +
+                            RandomGenerator.generateRating().floatValue() + ",\'" +
+                            RandomGenerator.generateCountry() + "\'," +
+                            "\'{\"" + genres.get(0) + "\",\"" + genres.get(1) + "\",\"" + genres.get(2) + "\"}\'" +
+                            "); ");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }).start();
+
+        new Thread(() -> {
+            // filling reviews
+            try {
+                Class.forName("org.postgresql.Driver");
+                Connection connection = DriverManager
+                        .getConnection("jdbc:postgresql://aws-us-east-1-portal.5.dblayer.com:18951/compose",
+                                "admin", "qwer1234");
+                Statement statement = connection.createStatement();
+                for (int i = 0; i < REVIEWS_COUNT; i++) {
+                    statement.execute("INSERT INTO moviefinder.reviews VALUES (" +
+                            i + ",\'" +
+                            RandomGenerator.generateDate() + "\'," +
+                            (random.nextInt(10) + 1) + ",\'" +
+                            RandomGenerator.generateText(300) + "\'" +
+                            "); ");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }).start();
+
+        new Thread(() -> {
+            // filling connections
+            try {
+                Class.forName("org.postgresql.Driver");
+                Connection connection = DriverManager
+                        .getConnection("jdbc:postgresql://aws-us-east-1-portal.5.dblayer.com:18951/compose",
+                                "admin", "qwer1234");
+                Statement statement = connection.createStatement();
+                for (int i = 0; i < MOVIES_COUNT; i++) {
+                    statement.execute("INSERT INTO moviefinder.films_people(film, person, kind) VALUES (" +
+                            i + "," +
+                            (random.nextInt(PEOPLE_COUNT / 2) + PEOPLE_COUNT / 2) +
+                            ",\'producer\'" +
+                            "); ");
+                    for (int j = 0; j < 3; j++) {
+                        statement.execute("INSERT INTO moviefinder.films_people(film, person, kind) VALUES (" +
+                                i + "," +
+                                (random.nextInt(PEOPLE_COUNT / 2)) +
+                                ",\'actor\'" +
+                                "); ");
+                    }
+                }
+
+                for (int i = 0; i < REVIEWS_COUNT; i++) {
+                    statement.execute("INSERT INTO moviefinder.films_reviews(film, review) VALUES (" +
+                            i + "," +
+                            random.nextInt(MOVIES_COUNT) +
+                            "); ");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }).start();
+
+        new Thread(() -> {
+            // filling id-counters
+            try {
+                Class.forName("org.postgresql.Driver");
+                Connection connection = DriverManager
+                        .getConnection("jdbc:postgresql://aws-us-east-1-portal.5.dblayer.com:18951/compose",
+                                "admin", "qwer1234");
+                Statement statement = connection.createStatement();
+                statement.execute("INSERT INTO moviefinder.counters(name, next_id) VALUES (" +
+                        "\'films\'," +
+                        MOVIES_COUNT +
+                        "); ");
+                statement.execute("INSERT INTO moviefinder.counters(name, next_id) VALUES (" +
+                        "\'people\'," +
+                        PEOPLE_COUNT +
+                        "); ");
+                statement.execute("INSERT INTO moviefinder.counters(name, next_id) VALUES (" +
+                        "\'reviews\'," +
+                        REVIEWS_COUNT +
+                        "); ");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }).start();
 
     }
 
